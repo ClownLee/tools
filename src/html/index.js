022bba2e240a3e1fe4b69b23c85882e73c674ac2
ele_11_ven <lwentao@hotmail.com>
2022-04-23 00:44:02 +0800

    html bug

--- src/html/index.js
+++ src/html/index.js
@@ -1,24 +1,17 @@
-const jsdom = require('jsdom')
-const { JSDOM } = jsdom;
-const { document } = (new JSDOM('')).window;
-
 const encode = (html) => {
-  var temp = document.createElement("div");
-  (temp.textContent != null) ? (temp.textContent = html) : (temp.innerText = html);
-  var output = temp.innerHTML;
-  temp = null;
-  return output;
+  var temp = document.createElement("div")
+  (temp.textContent != null) ? (temp.textContent = html) : (temp.innerText = html)
+  var output = temp.innerHTML
+  temp = null
+  return output
 }
 
 const decode = (text) => {
-  var temp = document.createElement("div");
-  temp.innerHTML = text;
-  var output = temp.innerText || temp.textContent;
-  temp = null;
-  return output;
+  var temp = document.createElement("div")
+  temp.innerHTML = text
+  var output = temp.innerText || temp.textContent
+  temp = null
+  return output
 }
 
-export default {
-  encode,
-  decode
-}
+export default { encode, decode }
